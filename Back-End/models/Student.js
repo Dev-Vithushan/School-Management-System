14c510f96b93f5263e6e61c4b4fbc39df46f929e
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-06-29 15:38:51 +0530

    Student model Created

---
+++ Back-End/models/Student.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema();
+
+
+const studentScheme = new Schema ({
+    firstname: {
+        type: String,
+        required: true,
+    },
+
+    lastname: {
+        type: String,
+        required: true,
+    },
+
+
+})
+
+module.exports = mongoose.model('Student', studentScheme);
+
+
