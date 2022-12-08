507fbd4d521d96b01921015bec341c46c6ea763e
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2022-12-08 16:15:26 +0530

    Added NAVBAR changes

--- pages/chat.js
+++ pages/chat.js
@@ -1,10 +1,15 @@
 import ChatView from "../Components/ChatView";
+import NavgBar from "../Layouts/NavgBar";
 
 export default function chat() {
   return (
+    <div style={{ display: "flex" }}>
+    <NavgBar/>
     <div style={{ margin: "20px 60px",width:"100vh" }}>
       <p style={{ fontSize: "30px" }}>Chat Area</p>
       <ChatView />
+    </div>
+
     </div>
   );
 }
